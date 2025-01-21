2745eaef5e1f465ff23b704a1ba29fedb6243bba
Ilya Shmyryov <119329448+ilyash0@users.noreply.github.com>
2025-01-21 11:12:23 +0500

    Add project files.

---
+++ To Do List/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.Security.Policy;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace To_Do_List
+{
+    public partial class MainWindow : Window
+    {
+        public List<ToDo> ToDoList { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ToDoList =
+            [
+                new ToDo("Поиграть в D&D", new DateTime(2024, 1, 20), "Dangeons And Dragons"),
+                new ToDo("Тест задача 2", new DateTime(1970, 1, 1), "Тест описание 2"),
+                new ToDo("Тест задача 3", new DateTime(1970, 1, 1), "тест описание 3")
+            ];
+
+            ClearGroupBoxToDo();
+            UpdateListToDo();
+        }
+
+        private void ClearGroupBoxToDo()
+        {
+            titleToDo.Text = null;
+            dateToDo.SelectedDate = new DateTime(2024, 1, 10);
+            descriptionToDo.Text = "Нет описания";
+        }
+
+        private void UpdateListToDo()
+        {
+            listToDo.ItemsSource = null;
+            listToDo.ItemsSource = ToDoList;
+        }
+
+        private void CheckBoxNewTask_Click(object sender, RoutedEventArgs e)
+        {
+            if (checkBoxNewTask.IsChecked == true)
+            {
+                groupBoxToDo.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                groupBoxToDo.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            ToDo task = new ToDo(
+            titleToDo.Text,
+            dateToDo.SelectedDate.Value,
+            descriptionToDo.Text
+            );
+            ToDoList.Add(task);
+
+            ClearGroupBoxToDo();
+            UpdateListToDo();
+        }
+
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            ToDoList.Remove(listToDo.SelectedItem as ToDo);
+
+            UpdateListToDo();
+        }
+    }
+}
---
+++ To Do List/ToDo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_Do_List
+{
+    public class ToDo
+    {
+        private string _name;
+        private DateTime _date;
+        private string _description;
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+        public DateTime Date
+        {
+            get => _date; 
+            set => _date = value;
+        }
+        public string Description
+        {
+            get => _description; 
+            set => _description = value; 
+        }
+
+        public ToDo()
+        {
+            _name = null;
+            _date = DateTime.Now;
+            _description = "Нет описания";
+        }
+
+        public ToDo(string name, DateTime date, string description)
+        {
+            Name = name;
+            Date = date;
+            Description = description;
+        }
+    }
+}
